28ca794fe43e05479886fe81403258d1340ea637
apple <apple@appledeMacBook-Pro.local>
2017-04-20 17:12:52 +0800

    check owner permission when access edit/update/destroy

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,4 +1,5 @@
 class GroupsController < ApplicationController
+  before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy]
   before_action :authenticate_user! , only: [:new, :create]
   before_action :authenticate_user! , only: [:new]
 
@@ -8,8 +9,12 @@ class GroupsController < ApplicationController
   def show
    @group = Group.find(params[:id])
   end
- def edit
-  @group = Group.find(params[:id])
+  def edit
+   @group = Group.find(params[:id])
+
+   if current_user != @group.user
+     redirect_to root_path, alert: "You have no permission."
+   end
  end
 
   def new
@@ -26,20 +31,30 @@ class GroupsController < ApplicationController
   end
 end
 
-  def update
-     @group = Group.find(params[:id])
-     if @group.update(group_params)
-       redirect_to groups_path, notice: "Update Success"
-     else
-       render :edit
-     end
+def update
+ @group = Group.find(params[:id])
+
+ if current_user != @group.user
+   redirect_to root_path, alert: "You have no permission."
+ end
+
+ if @group.update(group_params)
+   redirect_to groups_path, notice: "Update Success"
+ else
+   render :edit
+ end
+end
+def destroy
+   @group = Group.find(params[:id])
+
+   if current_user != @group.user
+     redirect_to root_path, alert: "You have no permission."
    end
-   def destroy
-    @group = Group.find(params[:id])
-    @group.destroy
-    flash[:alert] = "Group deleted"
-    redirect_to groups_path
-  end
+
+   @group.destroy
+   redirect_to groups_path, alert: "Group deleted"
+ end
+
 
  private
 
