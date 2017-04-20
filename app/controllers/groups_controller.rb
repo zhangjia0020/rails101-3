870ecd778a094666d52d8a1b94cd6d83e48f37d8
apple <apple@appledeMacBook-Pro.local>
2017-04-20 17:22:28 +0800

    use before_action to find_group_and_check_permission

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,7 @@
 class GroupsController < ApplicationController
   before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy]
+  before_action :find_group_and_check_permission, only: [:edit, :update, :destroy]
+
   before_action :authenticate_user! , only: [:new, :create]
   before_action :authenticate_user! , only: [:new]
 
@@ -10,12 +12,8 @@ class GroupsController < ApplicationController
    @group = Group.find(params[:id])
   end
   def edit
-   @group = Group.find(params[:id])
-
-   if current_user != @group.user
-     redirect_to root_path, alert: "You have no permission."
-   end
  end
+
 
   def new
    @group = Group.new
@@ -32,33 +30,29 @@ class GroupsController < ApplicationController
 end
 
 def update
- @group = Group.find(params[:id])
+    if @group.update(group_params)
+      redirect_to groups_path, notice: "Update Success"
+    else
+      render :edit
+    end
+  end
 
- if current_user != @group.user
-   redirect_to root_path, alert: "You have no permission."
- end
-
- if @group.update(group_params)
-   redirect_to groups_path, notice: "Update Success"
- else
-   render :edit
- end
-end
-def destroy
-   @group = Group.find(params[:id])
-
-   if current_user != @group.user
-     redirect_to root_path, alert: "You have no permission."
-   end
-
-   @group.destroy
-   redirect_to groups_path, alert: "Group deleted"
- end
+  def destroy
+    @group.destroy
+    redirect_to groups_path, alert: "Group deleted"
+  end
 
 
- private
+ def find_group_and_check_permission
+    @group = Group.find(params[:id])
 
- def group_params
-   params.require(:group).permit(:title, :description)
- end
+    if current_user != @group.user
+      redirect_to root_path, alert: "You have no permission."
+    end
+  end
+
+  def group_params
+    params.require(:group).permit(:title, :description)
+  end
+
 end
